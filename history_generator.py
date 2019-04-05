995c058d835c76fc4d345d5afd24cc36fb3bfd46
James Breslin <james.breslin@uconn.edu>
2019-04-05 12:30:37 -0400

    added logging

--- history_generator.py
+++ history_generator.py
@@ -1,9 +1,17 @@
 import psycopg2
 import psycopg2.extras
+import logging
+from datetime import datetime
 from configparser import ConfigParser
 from lib_bgp_data import Database
 
+LOG_LOCATION = r"/home/jab09044/HistoryLog/"
+
 def main():
+    t = datetime.now()
+    logging.basicConfig(level=logging.INFO, filename=LOG_LOCATION + t.strftime("%d_%m_%Y"))
+    logging.info("Start Time: " + t.strftime("%c"))
+
     cparser = ConfigParser()
     cparser.read("/etc/bgp/bgp.conf")
     
@@ -20,10 +28,9 @@ def main():
 
     # Generate Table
     sql_table = """CREATE TABLE IF NOT EXISTS prefix_origin_history (
-                    origin bigint,
-                    prefix cidr,
                     first_seen date DEFAULT current_date,
-                    history bigint,
+                    history bigint DEFAULT 1,
+                    LIKE validity INCLUDING prefix, asn AS origin
                     PRIMARY KEY (origin, prefix)
                 );"""
 
@@ -36,9 +43,9 @@ def main():
     
     # Case 2: Update record with 1 for seen prefix-origin
     sql_up1_record = """UPDATE prefix_origin_history 
-                        SET h.history = subquery.history*2+1
+                        SET history = subquery.history*2+1
                         FROM 
-                        (SELECT v.origin, v.prefix, hs.history 
+                        (SELECT hs.origin, hs.prefix, hs.history 
                         AS history FROM prefix_origin_history AS hs
                         INNER JOIN validity v
                         ON v.prefix = hs.prefix AND v.asn = hs.origin) 
@@ -55,9 +62,16 @@ def main():
                         AS subquery"""
     
     cur.execute(sql_table)
+    logging.info("Table creation completed.")
     cur.execute(sql_new_record)
+    logging.info("New entries added.")
     cur.execute(sql_up1_record)
+    logging.info("Seen pairs updated.")
     cur.execute(sql_up0_record)
+    logging.info("Unseen pairs updated.")
+
+
+    logging.info("End Time: " + datetime.now().strftime("%c"))
 
 if __name__=="__main__":
     main()
--- prefix_origin_history.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE prefix_origin_history (
-        origin bigint,
-	prefix cidr,
-        first_seen date DEFAULT current_date,
-	history bigint,
-        PRIMARY KEY (origin, prefix)
-);
